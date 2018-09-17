d1f9f7191aca2788f3523ce852a631dec48dfa6c
Terrance Corley <terrancecorley@outlook.com>
2018-09-17 15:07:33 -0700

    animated nav btn

--- scripts/index.js
+++ scripts/index.js
@@ -1,2 +1,16 @@
 // add event handler for role='button'- listen for space and enter trigger
 // DISREGARD PREV LINE IF USING BTN ELEMENT
+'use strict';
+
+const main = (() => {
+  const siteNavBtn = document.querySelector('#site-nav__menu-btn');
+
+  const navMenuTransition = (e) => {
+    // toggle class
+    // perform different transform depending on class
+    const elClasses = e.target.classList;
+    elClasses.toggle('close');
+  };
+
+  siteNavBtn.addEventListener('click', navMenuTransition);
+})();
